ed589f3bdbcfdc89b3c535649ea9d5db5a7e18b9
Carlos GIL <lao.tseu.is.alive@gmail.com>
2021-04-09 17:09:29 +0200

    first experiment with exoscale

---
+++ exoscale-create-vm/.terraform.lock.hcl
@@ -0,0 +1,21 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/exoscale/exoscale" {
+  version     = "0.18.2"
+  constraints = "0.18.2"
+  hashes = [
+    "h1:R2hmzYB+fLaB5wyU8SY+TuwiUGGbKA+mb1OCwwfbD04=",
+    "zh:0b038fc6eaf0e3c40cc2fe19e96dd55c266c9cf6f9b7af8527624b4a90329545",
+    "zh:2d3e56c71c78eaabe58f5dd8e51b3a291baa90e2be8d0e91483a7a501db29a9e",
+    "zh:327568e7a2bcb3be39845eb3784b819be66822fffb3f4e8f8a3b94496762d1c4",
+    "zh:40ae341fb442a32e6df477ef4c5b014742a3ccadda0b349ed06996f3fa599b0e",
+    "zh:4747256391ced19854087383f059bbc782bedb962061215e3759f40ccd0ca287",
+    "zh:5f3d7af0cd11e66f6db90dcaa330590b446554c590ed2459f404da5686fb3acb",
+    "zh:811155425e04757f719345888c61f03c4c2288c478e1458ab47db078208b4c15",
+    "zh:c517763525604f4152b240661d71e2be0eb730d3dfa704144c7adb81dfa4e2c4",
+    "zh:d18f2de11566a3831c688cf6c29e6c171bff95de592b3ae1faa2a184c5aa4a84",
+    "zh:e53414b5879b3840fddc6e4fda7fd14dbc694148bcbeba47c1ec63125b246de0",
+    "zh:f0f580667fe0f05909a880cffe2508386c3b8a0160b19959a51abb886a329bb4",
+  ]
+}
---
+++ exoscale-create-vm/main.tf
@@ -0,0 +1,72 @@
+variable "exoscale_api_key" { type = string }
+variable "exoscale_api_secret" { type = string }
+
+/****************************************************************
+how to create a simple vm on exoscale with terraform:
+  1) no need to prior login with exo cli, just have a IAM key/secret
+  2) cd in this directory and then the classic init/plan/apply
+  3) terraform init
+  4) terraform plan
+  5) terraform apply
+  6) terraform show
+  7) and if you want to cleanup everything : terraform destroy
+ 
+ to connect to the VM use the allocated ip from step 6: ssh ubuntu@194.182.163.23
+ you may need to create a FW rule to allow incoming ssh trafic
+ just go the exo web console --> compute/firewalling  and add a rule like :
+
+  INGRESS TCP CIDR YOUR_IP/32 TCP 22 - 22
+
+  or just click on the SSH button on the upper-right but be carefull 
+  because you will then allow every single ip on he "universe" to come in...
+
+ more information :
+ https://registry.terraform.io/providers/exoscale/exoscale/latest/docs/resources/compute
+****************************************************************/
+
+terraform {
+  required_providers {
+    exoscale = {
+      source  = "exoscale/exoscale"
+      version = "0.18.2"
+    }
+  }
+}
+
+provider "exoscale" {
+  key    = var.exoscale_api_key
+  secret = var.exoscale_api_secret
+}
+
+locals {
+  zone = "ch-gva-2"
+}
+
+data "exoscale_compute_template" "ubuntu" {
+  zone = local.zone
+  name = "Linux Ubuntu 20.04 LTS 64-bit"
+}
+
+resource "exoscale_compute" "my-server" {
+  zone         = local.zone
+  display_name = "my-server"
+  size         = "Small"
+  template_id  = data.exoscale_compute_template.ubuntu.id
+  disk_size    = 50
+  key_pair     = "cgil@vortex"
+  user_data    = <<EOF
+#cloud-config
+package_upgrade: true
+EOF
+
+tags = {
+    production = "true"
+    resource = "goeland"
+  }
+
+  timeouts {
+    create = "60m"
+    delete = "2h"
+  }
+
+}
